bc57ee66b67cf6171b53e9947c560707c86c55e4
agenciaweber-webmaster <66996105+agenciaweber-webmaster@users.noreply.github.com>
2025-12-22 23:20:30 -0300

    Add: message received route

--- backend/http/routers/router.go
+++ backend/http/routers/router.go
@@ -1,9 +1,19 @@
 package routers
 
-import "github.com/kataras/iris/v12"
+import (
+	"zubly/backend/http/controllers"
+
+	"github.com/kataras/iris/v12"
+)
 
 func Router(r *iris.Application) {
 	r.Any("/", func(ctx iris.Context) {
 		ctx.HTML("<h1>Hello World</h1>")
 	})
+
+	message := r.Party("/message")
+	{
+		message.Post("/received", controllers.MessageReceived)
+	}
+
 }
