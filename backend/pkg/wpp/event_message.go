8fd0b80a91c8b8de53890f4c7f579d29d0892dfc
agenciaweber-webmaster <66996105+agenciaweber-webmaster@users.noreply.github.com>
2026-05-19 19:37:27 -0300

    Create event_message.go

---
+++ backend/pkg/wpp/event_message.go
@@ -0,0 +1,34 @@
+package wpp
+
+type EventMessage []struct {
+	Body struct {
+		Data struct {
+			Info struct {
+				Chat        string `json:"Chat"`
+				ID          string `json:"ID"` // can be used by quotes
+				IsFromMe    bool   `json:"IsFromMe"`
+				IsGroup     bool   `json:"IsGroup"`
+				PushName    string `json:"PushName"`
+				Sender      string `json:"Sender"`    // default
+				SenderAlt   string `json:"SenderAlt"` // lid
+				Timestamp   string `json:"Timestamp"`
+				Type        string `json:"Type"`      // text, media
+				Mediatype   string `json:"Mediatype"` // "", image, audio, video, document
+				IsEdit      bool   `json:"IsEdit"`
+				IsEphemeral bool   `json:"IsEphemeral"`
+			} `json:"Info"`
+			Message struct {
+				Text                string `json:"conversation"`
+				ExtendedTextMessage struct {
+					ContextInfo struct {
+						QuotedMessageID string `json:"stanzaID"`
+						QuotedMessage   struct {
+							Text string `json:"conversation"`
+						} `json:"quotedMessage"`
+					} `json:"contextInfo"`
+				} `json:"extendedTextMessage"`
+			} `json:"Message"`
+		} `json:"data"`
+		EventData
+	} `json:"body"`
+}
