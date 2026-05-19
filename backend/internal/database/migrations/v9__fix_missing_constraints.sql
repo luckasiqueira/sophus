f79cc55a9ac07a0ea64667cdc54cd3d856f8399d
agenciaweber-webmaster <66996105+agenciaweber-webmaster@users.noreply.github.com>
2026-05-19 19:34:17 -0300

    Create v9__fix_missing_constraints.sql

---
+++ backend/internal/database/migrations/v9__fix_missing_constraints.sql
@@ -0,0 +1,17 @@
+ALTER TABLE IF EXISTS public.webhooks
+    ADD CONSTRAINT connection FOREIGN KEY (connectionid)
+        REFERENCES public.connections (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID;
+CREATE INDEX IF NOT EXISTS fki_connection
+    ON public.webhooks(connectionid);
+
+ALTER TABLE IF EXISTS public.connections
+    ADD CONSTRAINT company FOREIGN KEY (companyid)
+        REFERENCES public.companies (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID;
+CREATE INDEX IF NOT EXISTS fki_company
+    ON public.connections(companyid);
