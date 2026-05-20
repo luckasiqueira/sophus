8e4957dda387afc886abcf399ad8e4eb7c9d7fd9
agenciaweber-webmaster <66996105+agenciaweber-webmaster@users.noreply.github.com>
2026-05-20 14:00:25 -0300

    Update: creating contacts/conversations if inexistent

--- backend/internal/database/message_save.go
+++ backend/internal/database/message_save.go
@@ -2,15 +2,22 @@ package database
 
 import (
 	"encoding/json"
+	"fmt"
+	"time"
+
+	"strings"
 	"zubly/backend/pkg/wpp"
+
+	"github.com/google/uuid"
 )
 
 func MessageSave(connection Connection, msg wpp.EventMessage) error {
 	// Listar conversation mais recente e se ele está aberto. Se estiver fechado, criar um novo
-	conversationId, err := checkExistentConversation(msg)
+	conversationId, err := checkExistentConversation(connection.Id, msg)
 	if err != nil {
 		return err
 	}
+	fmt.Println("Starting message creation")
 	stmt, err := db.Prepare(`INSERT INTO public.messages (id, "messageId", text, "conversationId", "quotedId", "mediaType", "fullData", "createdAt", "updatedAt", 
      "isFromMe", "isGroup", "isRead", "isDeleted") VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 );`)
 	if err != nil {
@@ -45,28 +52,54 @@ func MessageSave(connection Connection, msg wpp.EventMessage) error {
 	return nil
 }
 
-func checkExistentConversation(msg wpp.EventMessage) (int, error) {
-	// extrair o contactID a partir do number/lid
+func checkExistentConversation(connectionId int, msg wpp.EventMessage) (int, error) {
 	stmt, err := db.Prepare(`SELECT "id" FROM "contacts" WHERE "number" = $1 OR "lid" = $2`)
 	if err != nil {
 		return 0, err
 	}
 	var contactId int
 	err = stmt.QueryRow(msg[0].Body.Data.Info.Sender, msg[0].Body.Data.Info.SenderAlt).Scan(&contactId)
-	if err != nil || contactId == 0 {
-		// criar contato se não existir
-		return 0, err
+	if err != nil {
+		if !strings.Contains(err.Error(), "no rows in result set") {
+			return 0, err
+		}
+		contact := Contact{
+			Name:         msg[0].Body.Data.Info.PushName,
+			Number:       strings.Split(msg[0].Body.Data.Info.Sender, "@")[0],
+			JID:          msg[0].Body.Data.Info.Sender,
+			LID:          msg[0].Body.Data.Info.SenderAlt,
+			ConnectionId: connectionId,
+			IsGroup:      msg[0].Body.Data.Info.IsGroup,
+		}
+		contactId, err = CreateContact(contact)
+		if err != nil {
+			return 0, err
+		}
 	}
 	defer stmt.Close()
 	stmtt, err := db.Prepare(`SELECT "id" FROM "conversations" WHERE "contactId" = $1 AND "status" = 'open'`)
 	if err != nil {
-		// criar conversation se não existir
 		return 0, err
 	}
 	var conversationId int
 	err = stmtt.QueryRow(contactId).Scan(&conversationId)
-	if err != nil || conversationId == 0 {
-		return 0, err
+	if err != nil {
+		if !strings.Contains(err.Error(), "no rows in result set") {
+			return 0, err
+		}
+		conversation := Conversation{
+			Status:       "open",
+			ContactID:    contactId,
+			ConnectionID: connectionId,
+			URL:          uuid.New(),
+			CreatedAt:    time.Now(),
+			UpdatedAt:    time.Now(),
+		}
+		conversationId, err = CreateConversation(conversation)
+		if err != nil {
+			return 0, err
+		}
 	}
+
 	return conversationId, nil
 }
