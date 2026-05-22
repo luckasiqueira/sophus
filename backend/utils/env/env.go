28893384d647bd4a3c378d94560d20c5f13251a0
agenciaweber-webmaster <66996105+agenciaweber-webmaster@users.noreply.github.com>
2026-05-21 23:47:59 -0300

    Update: now identify .env origin

--- backend/utils/env/env.go
+++ backend/utils/env/env.go
@@ -1,5 +1,33 @@
 package env
 
-import "github.com/joho/godotenv"
+import (
+	"os"
+	"strings"
 
-var Backend, _ = godotenv.Read(".env-backend", "env")
+	"github.com/joho/godotenv"
+)
+
+var Backend = envReader() //(".env-backend", "env")
+
+func envReader() map[string]string {
+	envList := make(map[string]string)
+	f, err := os.Open(".env")
+	defer f.Close()
+	if err != nil {
+		if strings.Contains(err.Error(), "no such file or directory") || strings.Contains(err.Error(), "cannot find the file") {
+			systemEnvs := os.Environ()
+			for _, env := range systemEnvs {
+				e := strings.SplitN(env, "=", 2)
+				key, value := e[0], e[1]
+				envList[key] = value
+			}
+			return envList
+		}
+	}
+	envList, err = godotenv.Read(".env")
+	if err != nil {
+		//fmt.Println(err)
+		os.Exit(1)
+	}
+	return envList
+}
