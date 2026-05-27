11fc302cd9614eb75d9a8b79e7fbe12a7c2de929
agenciaweber-webmaster <66996105+agenciaweber-webmaster@users.noreply.github.com>
2026-05-27 10:33:15 -0300

    New: run migrations before start

--- backend/backend.go
+++ backend/backend.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"sophus/backend/internal/repo"
 	"sophus/backend/pkg/http"
 	"sophus/backend/utils/env"
 )
 
 func main() {
-	//err := repo.RunMigrations()
-	//if err != nil {
-	//	fmt.Println(err)
-	//	os.Exit(1)
-	//}
-	err := http.Start(env.Backend["SERVER_PORT"])
+	err := repo.RunMigrations()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = http.Start(env.Backend["SERVER_PORT"])
 	if err != nil {
 		panic(err)
 	}
